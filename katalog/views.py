cc1cc60f760aa0971c4bc86327d3eae7931b5733
Ghazian Gunawan <112075463+ghaziangunawan@users.noreply.github.com>
2022-09-13 23:12:02 +0700

    update

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_katalog_item = CatalogItem.objects.all()
+    context = {
+        'list_item': data_katalog_item,
+        'name': 'Muhammad Ghazian Gunawan',
+        'student_id': '2106656522'
+    }
+    return render(request, 'katalog.html',context)
