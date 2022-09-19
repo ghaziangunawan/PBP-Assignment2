2ac7dfcea0db8a6a080bbd5d10b08dbecd79fa32
Ghazian Gunawan <112075463+ghaziangunawan@users.noreply.github.com>
2022-09-19 20:07:28 +0700

    mywatchlist app

---
+++ mywatchlist/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from mywatchlist.views import show_watchlist,show_json,show_xml
+
+
+app_name = 'mywatchlist'
+
+urlpatterns = [path('', show_watchlist, name='show_watchlist'),
+    path('html/', show_watchlist, name='show_watchlist'),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/<int:id>', show_json, name= 'show_json'),
+
+    ]
+    
+    
---
+++ mywatchlist/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from mywatchlist.models import watchlistItems
+from django.http import HttpResponse
+from django.core import serializers
+
+# Create your views here.
+def show_watchlist(request):
+    data_watchlist_item = watchlistItems.objects.all()
+    context = {
+        'list_item': data_watchlist_item,
+        'name': 'Muhammad Ghazian Gunawan',
+        'student_id': '2106656522'
+    }
+
+    return render(request, 'watchlist.html',context)
+
+
+
+def show_xml(request):
+    data = watchlistItems.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request, id):
+    data = watchlistItems.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
