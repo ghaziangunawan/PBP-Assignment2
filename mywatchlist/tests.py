1b49817716b582cca60e2aebdf46a555a5bf49ee
Ghazian Gunawan <112075463+ghaziangunawan@users.noreply.github.com>
2022-09-20 23:23:32 +0700

    create testing

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+
+class watchlistTest(TestCase):
+    def test_url_exist(self):
+        resp = self.client.get('/mywatchlist/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_json_exist(self):
+        resp = self.client.get('/mywatchlist/json/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_xml_exist(self):
+        resp = self.client.get('/mywatchlist/xml/')
+        self.assertEqual(resp.status_code, 200)
+
+    
